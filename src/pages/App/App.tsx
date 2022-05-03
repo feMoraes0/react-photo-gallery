8ca144dc1f767db19a02057f2fa89326ec54992b
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-05-03 17:06:41 -0300

    feat: adds unsplash api call to app

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,38 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Container, Form, FormButton, FormInput, Grid, GridColumn, Image, Title,
 } from './App.style';
 
-const App:React.ElementType = () => (
-  <Container>
-    <Title>Unsplash Gallery</Title>
-    <Form>
-      <FormInput placeholder="Type a filter" />
-      <FormButton>Search</FormButton>
-    </Form>
-    <Grid>
-      <GridColumn>
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150x200" />
-        <Image src="https://via.placeholder.com/150x100" />
-        <Image src="https://via.placeholder.com/150x300" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150x400" />
-        <Image src="https://via.placeholder.com/100x50" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150x200" />
-        <Image src="https://via.placeholder.com/150x100" />
-        <Image src="https://via.placeholder.com/150x300" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150" />
-        <Image src="https://via.placeholder.com/150x400" />
-        <Image src="https://via.placeholder.com/100x50" />
-        <Image src="https://via.placeholder.com/150" />
-      </GridColumn>
-    </Grid>
-  </Container>
-);
+const App:React.ElementType = () => {
+  const [photos, setPhotos] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    const accessKey = process.env.REACT_APP_UNSPLASH_KEY;
+
+    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+      .then((response) => response.json())
+      .then((data) => setPhotos(data));
+  }, []);
+
+  return (
+    <Container>
+      <Title>Unsplash Gallery</Title>
+      <Form>
+        <FormInput placeholder="Type a filter" />
+        <FormButton>Search</FormButton>
+      </Form>
+      <Grid>
+        <GridColumn>
+          {
+            photos.map((photo) => (
+              <Image key={photo.id} src={photo.urls.raw} alt={photo.description} />
+            ))
+          }
+        </GridColumn>
+      </Grid>
+    </Container>
+  );
+};
 
 export default App;
