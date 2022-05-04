1acc0a7363efbd38f4affb133d271e333c183ad9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-05-03 21:22:21 -0300

    test: adds unit test to validate heading text

--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -1,5 +1,20 @@
+/* eslint-disable no-undef */
+import { act, render, screen } from '@testing-library/react';
 import React from 'react';
+import App from './App';
 
-test('Should 2 + 2 be equal 4', () => {
-  expect(2 + 2).toBe(4);
+describe('App component', () => {
+  it('Given a component rendered, when a h1 is found, then should have text content with application title', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue([]),
+    } as unknown as Response);
+    // GIVEN
+    await act(() => {
+      render(<App />);
+    });
+    // WHEN
+    const appTitle = screen.getByRole('heading', { level: 1 });
+    // THEN
+    expect(appTitle).toHaveTextContent('Unsplash Gallery');
+  });
 });
