7254e4becfa9d243ba185c1f4b15b72f9ffba0ec
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-05-04 11:30:54 -0300

    test: updates App.ts test coverage to cover images and API

--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -17,4 +17,58 @@ describe('App component', () => {
     // THEN
     expect(appTitle).toHaveTextContent('Unsplash Gallery');
   });
+
+  it('Given a component rendered, when API returns an element, then should image rendered have returned element properties', async () => {
+    const response = [
+      {
+        id: '1',
+        urls: {
+          raw: 'http://localhost/image-1',
+        },
+        description: 'any description',
+      },
+    ];
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(response),
+    } as unknown as Response);
+    // GIVEN
+    await act(() => {
+      render(<App />);
+    });
+    // WHEN
+    const image = screen.getByRole('img');
+    // THEN
+    expect(image).toHaveProperty('src', response[0].urls.raw);
+    expect(image).toHaveProperty('alt', response[0].description);
+  });
+
+  it('Given a component rendered, when API returns multiple elements, then should find the same number of images', async () => {
+    const response = [
+      {
+        id: '1',
+        urls: {
+          raw: 'http://localhost/image-1',
+        },
+        description: 'any description',
+      },
+      {
+        id: '2',
+        urls: {
+          raw: 'http://localhost/image-2',
+        },
+        description: 'any description 2',
+      },
+    ];
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(response),
+    } as unknown as Response);
+    // GIVEN
+    await act(() => {
+      render(<App />);
+    });
+    // WHEN
+    const images = screen.getAllByRole('img');
+    // THEN
+    expect(images.length).toBe(response.length);
+  });
 });
