cb4f99ba10a1c846f9f687eb93d881301ab4d400
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-05-07 22:05:00 -0300

    refactor: updates App with new methods to get the photos

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import FetchPhotosService from '../../services/fetch/FetchPhotosService';
+import GetPhotosUsecase from '../../usecase/get-photos-usecase';
 import {
   Container, Form, FormButton, FormInput, Grid, GridColumn, Image, Title,
 } from './App.style';
 
+interface PhotosData {
+  id: string,
+  url: string,
+  description?: string
+}
+
 const App:React.ElementType = () => {
-  const [photos, setPhotos] = useState<Array<any>>([]);
+  const photosService = new FetchPhotosService();
+  const getPhotosUsecase = new GetPhotosUsecase(photosService);
+  const [photos, setPhotos] = useState<Array<PhotosData>>([]);
 
   useEffect(() => {
     const accessKey = process.env.REACT_APP_UNSPLASH_KEY;
+    const url = `https://api.unsplash.com/photos/?client_id=${accessKey}`;
 
-    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
-      .then((response) => response.json())
+    getPhotosUsecase.execute({ url })
       .then((data) => setPhotos(data));
   }, []);
 
@@ -25,7 +35,7 @@ const App:React.ElementType = () => {
         <GridColumn>
           {
             photos.map((photo) => (
-              <Image key={photo.id} src={photo.urls.raw} alt={photo.description} />
+              <Image key={photo.id} src={photo.url} alt={photo.description} />
             ))
           }
         </GridColumn>
